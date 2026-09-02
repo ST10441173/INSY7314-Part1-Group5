0892e725d707f5c56e9ebb36d331651607969476
Muhammad Seedat <seedat@zyrobit.co.za>
2026-09-02 20:41:30 +0200

    Trim comments in auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,15 +4,8 @@ const { registerUser, loginUser, getMe } = require('../controllers/authControlle
 const { protect } = require('../middleware/authMiddleware');
 const { registerValidationRules, loginValidationRules } = require('../middleware/validateMiddleware');
 
-// Register a new account. Input is validated/sanitised first.
 router.post('/register', registerValidationRules, registerUser);
-
-// Log in with email + password, receive a JWT.
-// Note: rate limiting on this endpoint is a Part 2 requirement, added
-// once express-rate-limit is introduced alongside the rest of the API.
 router.post('/login', loginValidationRules, loginUser);
-
-// Example protected route - requires a valid JWT (see authMiddleware.protect).
 router.get('/me', protect, getMe);
 
 module.exports = router;
